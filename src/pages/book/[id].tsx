2c3a9c29f5b3da55c98e9b0fb709cb68b0df7503
backms <iphonex@kakao.com>
2024-11-29 20:21:29 +0900

    ssr 적용

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,4 +1,6 @@
 import style from './[id].module.css';
+import {GetServerSidePropsContext, InferGetServerSidePropsType} from "next";
+import fetchOneBook from "@/lib/fetch-one-book";
 
 const mockData = {
     "id": 1,
@@ -10,7 +12,22 @@ const mockData = {
     "coverImgUrl": "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg"
 }
 
-export default function Page() {
+export const getServerSideProps = async(context: GetServerSidePropsContext) => {
+
+    const id = context.params!.id;
+    const book = await fetchOneBook(Number(id));
+
+    return {
+        props: {
+            book,
+        }
+    };
+};
+
+export default function Page({
+                                 book,
+                             }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    if (!book) return "문제가 발생했습니다 다시 시도하세요";
 
     const {
         id,
@@ -19,8 +36,8 @@ export default function Page() {
         description,
         author,
         publisher,
-        coverImgUrl
-    } = mockData;
+        coverImgUrl,
+    } = book;
 
     return <div className={style.container}>
         <div
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,47 @@
 // CSS Module
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
-import {ReactNode} from "react";
+import {ReactNode, useEffect} from "react";
 import books from '@/mock/books.json';
-import BookItem from "@/components/book-item";   // @ 는 src 를 가르킴.
+import BookItem from "@/components/book-item";
+import {InferGetStaticPropsType} from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";   // @ 는 src 를 가르킴.
 
-export default function Home() {
+// 서버 사이드 함수를 생성함으로써 서버 사이드 렌더링 가능.
+export const getServerSideProps = async() => {
+
+    // 직렬 조회
+    // const allBooks = await fetchBooks();
+    // const recoBooks = await fetchRandomBooks();
+
+    // 병렬 조회
+    const [allBooks, recoBooks] = await Promise.all([
+        fetchBooks(),
+        fetchRandomBooks(),
+    ]);
+
+    return {
+        props: {    // 반드시 props 객체에 데이터를 담아서 리턴해야함.
+            allBooks,
+            recoBooks,
+        }
+    }
+};
+
+export default function Home({ allBooks, recoBooks }: InferGetStaticPropsType<typeof getServerSideProps>) {
+
   return (
       <div className={style.container}>
           <section>
               <h3>지금 추천하는 도서</h3>
-              {books.map(book => (
+              {recoBooks.map(book => (
                   <BookItem key={book.id} {...book} />
               ))}
           </section>
           <section>
               <h3>등록된 모든 도서</h3>
-              {books.map(book => (
+              {allBooks.map(book => (
                   <BookItem key={book.id} {...book} />
               ))}
           </section>
